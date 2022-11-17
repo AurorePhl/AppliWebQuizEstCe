baddc381b1c5e0c3e07924f3629f61a7a4d91ace
aurore.philippe <asiphilippe@hotmail.com>
2022-11-17 10:29:19 +0100

    creation partie front

---
+++ AppliWebQuizEstCe/src/main/java/com/projetS5/AppliWebQuizEstCe/AppliWebQuizEstCeApplication.java
@@ -0,0 +1,13 @@
+package com.projetS5.AppliWebQuizEstCe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliWebQuizEstCeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppliWebQuizEstCeApplication.class, args);
+	}
+
+}
---
+++ AppliWebQuizEstCe/src/test/java/com/projetS5/AppliWebQuizEstCe/AppliWebQuizEstCeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetS5.AppliWebQuizEstCe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliWebQuizEstCeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
