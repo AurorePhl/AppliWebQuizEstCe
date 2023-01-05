6d9da953170654c609fbcef9e2edc6ebdadeb57d
aurore.philippe <asiphilippe@hotmail.com>
2023-01-05 12:39:33 +0100

    coms

---
+++ AppliWebQuizEstCe/src/main/java/com/projetS5/AppliWebQuizEstCe/Controller/GameController.java
@@ -0,0 +1,51 @@
+package com.projetS5.AppliWebQuizEstCe.Controller;
+
+import com.projetS5.AppliWebQuizEstCe.models.MotCles;
+import com.projetS5.AppliWebQuizEstCe.models.Piece;
+import com.projetS5.AppliWebQuizEstCe.models.Questions;
+import com.projetS5.AppliWebQuizEstCe.models.Reponse;
+import com.projetS5.AppliWebQuizEstCe.models.Theme;
+
+public class GameController {
+
+	Questions questions;
+	String questionChoisie;
+	MotCles motCleChoisi;
+	Piece plateau;
+	Reponse res;
+	Theme theme;
+	
+	public void ChoixQuestion() {
+		/* début dans WindowQuestion
+		 * Choix question (clique sur bouton) 
+		-> Ajout dans questions posées 
+		-> obtenir pièces concernées grâce aux réponses   
+		-> pièces concernées grisées (value replace à 1 dans nomPieces) 
+		redirect vers WindowKeyWord 
+		si plus qu'une pièce -> Victory() */
+	}
+	
+	public void ChoixMotCle() {
+		/* début dans WindowKeyword
+		 * Choix motCle (clique sur bouton)  
+		 * -> Ajout value +1 dans map motclés  
+		 * -> récupérer questions associées  
+		 * -> redirect vers WindowQuestion*/ 
+	}
+	
+	public void ChoixPiece() {
+		/* (début dans WindowPiece ou bien (WindowKeyword ou WindowQuestion))
+		 * Choix pièce (changer valeur de PièceChoisie) 
+		 * -> Pièces concernées grisées (value replace à 1 dans nomPieces) 
+		 * -> Victory() */
+	}
+	
+	public void Victory() {
+		/* Si pieceChoisie identique à pieceGagnante 
+		 * -> redirect vers WindowVictory 
+		 * -> le joueur clique sur "suivant" 
+		 * -> redirection vers page d'accueil
+		 * Sinon 
+		 * -> pieceChoisie : piece grisée (value replace à 1 dans nomPieces correspondant) */
+	}
+}
---
+++ AppliWebQuizEstCe/src/main/java/com/projetS5/AppliWebQuizEstCe/Controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.projetS5.AppliWebQuizEstCe.Controller;
+
+import java.util.ArrayList;
+
+import com.projetS5.AppliWebQuizEstCe.models.Pseudo;
+import com.projetS5.AppliWebQuizEstCe.models.Theme;
+
+public class HomeController {
+
+	Theme themeChoisi; 
+	Pseudo pseudo;
+	
+	public void ChoixPseudo(String pseudo){
+		/* Entrée du pseudo dans la zone de text (récupérer text entrer) 
+		 * -> le joueur clique sur "valider" 
+		 * -> redirection vers choix du thème */
+	}
+	
+	public void ChoixTheme(String Theme) {
+		/* Choix du thème 
+		 * -> le joueur clique sur le bouton "commencer/start" à coté du theme souhaité  
+		 * -> redirection vers page du jeu*/
+	}
+	
+}
