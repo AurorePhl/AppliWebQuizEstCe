e6a45adc77e23ac401d2271dbd49b845b959826e
ChaimaeH2 <116259017+ChaimaeH2@users.noreply.github.com>
2023-01-12 19:33:03 +0100

    Update Theme.java

--- AppliWebQuizEstCe/src/main/java/com/projetS5/AppliWebQuizEstCe/models/Theme.java
+++ AppliWebQuizEstCe/src/main/java/com/projetS5/AppliWebQuizEstCe/models/Theme.java
@@ -1,5 +1,32 @@
 package com.projetS5.AppliWebQuizEstCe.models;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Theme {
-
+	private String currentTheme;
+	private HashMap <String,ArrayList<ButeursFrancais>> themes;
+	
+	public Theme () {
+		themes = new HashMap<String,ArrayList <ButeursFrancais>>();
+		currentTheme = "";
+	}
+	//Ajout des thèmes avec les questions associées
+	public void addTheme(String theme,ArrayList<ButeursFrancais> Buteurs) {
+		themes.put(theme,ButeursFrancais);
+		
+	}
+	//Methode pour definir le theme 
+	public void setcurrentTheme(String theme) {
+		currentTheme=theme;
+	
+	}
+	//Methode pour obtenir le thème actuel
+	public String getCurrentTheme() {
+		return currentTheme; 
+		
+	}
+	//obtenir les questions associées au thème actuel
+	 public ArrayList<ButeursFrancais> getButeursFrancaisForCurrentTheme(){
+	        return themes.get(currentTheme);
+	    }
 }
